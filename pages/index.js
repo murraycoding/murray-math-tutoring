f90ec38da909e7590b24294a54801d556b8daa68
MurrayCoding <murraycoding@gmail.com>
2021-03-11 09:05:09 -0500

    added online tutoring image component

--- pages/index.js
+++ pages/index.js
@@ -7,41 +7,35 @@ export default function Home() {
   return (
     <>
       <ImageSection
-      src="/APlus.png"
-      alt=""
-      height={162}
-      width={156}
-      title="Welcome!"
-      content="Murray Math Tutoring is an online math tutoring service run by a husband and wife team of certified teachers. For more information, please see our about section."
-      link="about"
-      btnText="about"
+        src="/APlus.png"
+        alt=""
+        height={162}
+        width={156}
+        title="Welcome!"
+        content="Murray Math Tutoring is an online math tutoring service run by a husband and wife team of certified teachers. For more information, please see our about section."
+        link="about"
+        btnText="about"
       />
       <ImageSection
-      src="/Free.png"
-      alt=""
-      height={162}
-      width={156}
-      title="Your first hour is FREE!"
-      content="Yes! You read that correctly. We believe in what we do so much we ALWAYS give the first hour of tutoring for free to any new students. Contact us today to schedule your first free hour and see if Murray Math Tutoring is right for you!"
-      link="about"
-      btnText="about"
-      />
-      {/* <ImageSection
-      src="test image 2"
-      alt=""
-      title="Free Tutoring"
-      content="Yes! You read that correctly. We believe in what we do so much we ALWAYS give the first hour of tutoring for free to any new students. Contact us today to schedule your first free hour and see if Murray Math Tutoring is right for you!"
-      link="contact"
-      btnText="contact"
+        src="/Free.png"
+        alt=""
+        height={162}
+        width={156}
+        title="Your first hour is FREE!"
+        content="Yes! You read that correctly. We believe in what we do so much we ALWAYS give the first hour of tutoring for free to any new students. Contact us today to schedule your first free hour and see if Murray Math Tutoring is right for you!"
+        link="about"
+        btnText="about"
       />
       <ImageSection
-      src="test image 3"
-      alt=""
+      src="/Online.png"
+      alt="online"
+      height={162}
+      width={156}
       title="How does online tutoring work?"
       content="This will be filled in later. This is another change."
       link="contact"
       btnText="contact"
-      /> */}
+      />
     </>
   )
 }
