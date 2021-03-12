e00523d1008f1d1d6b83ff47a5dfcd48efb98f36
MurrayCoding <murraycoding@gmail.com>
2021-03-12 14:34:05 -0500

    update to homepage/imageSection fixes #2

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
         width={156}
         title="Your first hour is FREE!"
         content="Yes! You read that correctly. We believe in what we do so much we ALWAYS give the first hour of tutoring for free to any new students. Contact us today to schedule your first free hour and see if Murray Math Tutoring is right for you!"
-        link="about"
-        btnText="about"
+        link="free-tutoring"
+        btnText="Free Tutoring"
       />
       <ImageSection
         src="/Online.png"
@@ -33,8 +33,8 @@ export default function Home() {
         width={156}
         title="How does online tutoring work?"
         content="This will be filled in later. This is another change."
-        link="contact"
-        btnText="contact"
+        link="online-tutoring"
+        btnText="online tutoring"
       />
     </>
   )
