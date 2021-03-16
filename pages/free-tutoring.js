57dd46d0ebf20c19a789ee4c244687aa787a4df7
MurrayCoding <murraycoding@gmail.com>
2021-03-15 22:12:22 -0400

    updated content for pages

--- pages/free-tutoring.js
+++ pages/free-tutoring.js
@@ -2,6 +2,6 @@ import ImageSection from '../components/ImageSection'
 
 export default function FreeTutoringPage() {
   return (
-    <p>This is the free tutoring page.</p>
+    <p>Create content for the page which demonstrates how a free session is set up and started. Also link to the online tutoring page for directions on how to do online tutoring.</p>
   )
 }
--- pages/online-tutoring.js
+++ pages/online-tutoring.js
@@ -3,7 +3,7 @@ import ImageSection from '../components/ImageSection'
 export default function OnlineTutoring() {
   return (
     <>
-      <p>This is the online tutoring page.</p>
+      <p>Things to include, online tutoring testimonials, directions on how to start an online tutoring session. Links to important resources used during online tutoring.</p>
     </>
   )
 }
