1abed54cbdfa107f90ce5de79489b46f97f87bd4
MurrayCoding <murraycoding@gmail.com>
2021-03-17 10:26:42 -0400

    added header to online tutoring page

--- pages/online-tutoring.js
+++ pages/online-tutoring.js
@@ -3,6 +3,8 @@ import ImageSection from '../components/ImageSection'
 export default function OnlineTutoring() {
   return (
     <>
+      <h1 className="sectionTitle">Online Tutoring</h1>
+      <p>Below is a description of how online tutoring works and what resources we use to best help the students we work with. You will also find important links to resources we often use during online tutoring. Please click <a className="hoverLink" href="https://drive.google.com/file/d/1eh1307kQlF_0AQ0UuHJeyuK-xwvjLf_d/view?usp=sharing" target="_blank">HERE</a> for a link to our general online tutoring instrcutions.</p>
       <ImageSection 
         src="/MicrosoftSurface.jpg"
         alt="microsoft-surface"
@@ -21,7 +23,7 @@ export default function OnlineTutoring() {
         content="We use a professional condensing microphone for premium audio quality. Students can hear us very clearly without background noise. This makes focusing on the lesson that much easier for students."
         link="none"
       />
-      <p>This is the <a className="hoverLink" href="https://drive.google.com/file/d/1eh1307kQlF_0AQ0UuHJeyuK-xwvjLf_d/view?usp=sharing" target="_blank">LINK</a> for the online tutoring instructions.</p>
+      <p>This is the  for the online tutoring instructions.</p>
     </>
   )
 }
