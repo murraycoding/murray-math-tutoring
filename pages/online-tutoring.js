c972723aebe09c06904f85ed01fedd7ea6ca3447
MurrayCoding <murraycoding@gmail.com>
2021-03-16 21:44:14 -0400

    add link to online tutoring instructions pdf

--- pages/online-tutoring.js
+++ pages/online-tutoring.js
@@ -3,7 +3,7 @@ import ImageSection from '../components/ImageSection'
 export default function OnlineTutoring() {
   return (
     <>
-      <p>Things to include, online tutoring testimonials, directions on how to start an online tutoring session. Links to important resources used during online tutoring.</p>
+      <p>This is the <a href="https://drive.google.com/file/d/1eh1307kQlF_0AQ0UuHJeyuK-xwvjLf_d/view?usp=sharing" target="_blank">LINK</a> for the online tutoring instructions.</p>
     </>
   )
 }
