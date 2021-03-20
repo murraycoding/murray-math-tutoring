735e536926534b3e0ba259875d085d4420484a52
MurrayCoding <murraycoding@gmail.com>
2021-03-20 16:42:44 -0400

    listed what to add to free tutoring page #5

--- pages/free-tutoring.js
+++ pages/free-tutoring.js
@@ -2,6 +2,9 @@ import ImageSection from '../components/ImageSection'
 
 export default function FreeTutoringPage() {
   return (
-    <p>Create content for the page which demonstrates how a free session is set up and started. Also link to the online tutoring page for directions on how to do online tutoring.</p>
+    <>
+      <p>Create content for the page which demonstrates how a free session is set up and started. Also link to the online tutoring page for directions on how to do online tutoring.</p>
+      <p>Include a list of steps parents need to take to schedule their first free hour. Also include a disclaimer that it is based on availability.</p>
+    </>
   )
 }
