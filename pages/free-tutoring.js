4306777bf0bec7f5fc2e42a9fa5366be9302addf
MurrayCoding <murraycoding@gmail.com>
2021-03-23 08:30:36 -0400

    updated content on free tutoring page

--- pages/free-tutoring.js
+++ pages/free-tutoring.js
@@ -3,8 +3,19 @@ import ImageSection from '../components/ImageSection'
 export default function FreeTutoringPage() {
   return (
     <>
-      <p>Create content for the page which demonstrates how a free session is set up and started. Also link to the online tutoring page for directions on how to do online tutoring.</p>
-      <p>Include a list of steps parents need to take to schedule their first free hour. Also include a disclaimer that it is based on availability.</p>
+      <p>We believe in our services so much that we ALWAYS give the first hour for free. We also give the first hour for free for a few main reasons:</p>
+      <ul className="centerlist">
+        <li>To make sure that your child feels comfortable working with the tutor.</li>
+        <li>To know the value of the services we are providing.</li>
+        <li>To experience what online tutoring is like.</li>
+      </ul>
+      <p>To set up your first free hour please follow these steps.</p>
+      <ul className="centerlist">
+        <li>Get in contact with us by phone or email.</li>
+        <li>Let us know what times work with your schedule.</li>
+        <li>Experience the difference in online tutoring with professional educators.</li>
+      </ul>
+      <p>Please note that free hours are given based on availability in our scheulde. Throughout the school year, our schedule changes due to our students' schedules changing. If a spot is not available, please continue to check in with us to see what our open timeslots are.</p>
     </>
   )
 }
