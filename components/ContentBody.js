caa98414a5a0850408d3265e27df51ea797f0a2f
MurrayCoding <murraycoding@gmail.com>
2021-03-23 14:24:57 -0400

    updated background image

--- components/ContentBody.js
+++ components/ContentBody.js
@@ -3,19 +3,8 @@ import ContentBodyStyles from '../styles/ContentBody.module.scss'
 
 export default function ContentBody({ children }) {
   return(
-    <div className={ContentBodyStyles.wrapper}>
+    <div className={ContentBodyStyles.newBackground}>
       { children }
-      <div className={ContentBodyStyles.backgroundimage}>
-        <Image
-          src="/Paper.jpg"
-          alt=""
-          width={2000}
-          height={1500}
-          quality={25}
-          layout="fixed"
-          objectPosition="left top"
-        />
-      </div>
     </div>
   )
 }
